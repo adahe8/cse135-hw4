690b8c89d76207c848cf2a3f6cb74d3bdd8d89dd
Ada He <ada.he0908@gmail.com>
2026-03-06 23:53:51 -0800

    Styled form to test server side scripting endpoints. Also created additional form for collecting reading preferences, and added a CSS graphic that will echo back data submitted from that form as an additional interaction.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Form handling
-const form = document.getElementById('echo-form');
+const echoform = document.getElementById('echo-form');
 const endpoints = {
     'PHP': '/cgi-bin/echo-php.php',
     'Ruby': '/cgi-bin/echo-ruby.rb',
     'Python': '/cgi-bin/echo-python.py'
 }
 
-form.addEventListener('submit', sendRequest);
+echoform.addEventListener('submit', sendRequest);
 
 function sendRequest(event){
     event.preventDefault();
@@ -15,30 +15,30 @@ function sendRequest(event){
     let selectedLang = document.getElementById('scripting-lang').value;
     let selectedMethod = document.getElementById('http-method').value;
     document.getElementById('override').value = selectedMethod;
-    const data = getFormData(form);
+    const data = getFormData(echoform);
 
     // Create user identification for LogRocket
-    LogRocket.identify(form.name || 'ANONYMOUS_USER', {
-        name: form.name || 'Anonymous',
+    LogRocket.identify(echoform.name || 'ANONYMOUS_USER', {
+        name: echoform.name || 'Anonymous',
     });
 
-    // simple analytics custom event for each form s submit button clicks
+    // simple analytics custom event for each form's submit button clicks
     sa_event('request_submission');
     
     const action = endpoints[selectedLang];
     if (!action) throw new Error('Invalid language selected! Please stick to the original dropdown options.');
 
     if (selectedMethod === 'PUT' || selectedMethod === 'DELETE') {
-        form.method = 'POST';
+        echoform.method = 'POST';
     } else if (selectedMethod === 'GET' || selectedMethod === 'POST'){
-        form.method = selectedMethod;
+        echoform.method = selectedMethod;
     } else {
          throw new Error('Invalid request type! Please stick to the original dropdown options.');
     }
-    form.action = action;
+    echoform.action = action;
     
     if (encodeAsJSON) {
-        fetch(form.action, {
+        fetch(echoform.action, {
             method: (selectedMethod === 'PUT' || selectedMethod === 'DELETE') ? 'POST' : selectedMethod,
             headers: {
                 'Content-Type': 'application/json'
@@ -54,10 +54,25 @@ function sendRequest(event){
             console.error('Error:', error);
         });
     } else {
-        form.submit();
+        echoform.submit();
     }
 }
 
 function getFormData(form){
     return Object.fromEntries(new FormData(form).entries());
+}
+
+const bookform = document.getElementById('book-form');
+echoform.addEventListener('submit', echoData);
+const cover = document.getElementByAttribute('.cover');
+const title = document.getElementById('booktitle');
+const genres = document.getElementById('genres');
+
+function echoData(event){
+    event.preventDefault();
+    
+    const formData = new FormData(bookform);
+    title.innerHTML = formData.get('book');
+    genres.innerHTML = formData.getAll('genre').join(', ');
+    cover.style.backgroundColor = "lightblue";
 }
